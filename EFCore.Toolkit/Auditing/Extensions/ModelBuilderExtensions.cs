183b9a93379994b6b58882bc38a7b702258b4c5d
Thomas Galliker <thomas@bluewin.ch>
2019-04-10 15:50:38 +0200

    Add Remove methods to remove conventions (similar to EF6)

--- EFCore.Toolkit/Auditing/Extensions/ModelBuilderExtensions.cs
+++ EFCore.Toolkit/Auditing/Extensions/ModelBuilderExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EFCore.Toolkit.Auditing.Extensions
 {
@@ -11,6 +12,22 @@ namespace EFCore.Toolkit.Auditing.Extensions
         public static void AddConfiguration<TEntity>(this ModelBuilder modelBuilder, EntityTypeConfiguration<TEntity> entityConfiguration) where TEntity : class
         {
             modelBuilder.Entity<TEntity>(entityConfiguration.Configure);
+        }
+
+        public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.EntityTypes().Configure(et => et.Relational().TableName = et.DisplayName());
+        }
+
+        public static void RemoveCascadeDeleteConvention(this ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.EntityTypes())
+            {
+                entityType.GetForeignKeys()
+                    .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+                    .ToList()
+                    .ForEach(fk => fk.DeleteBehavior = DeleteBehavior.Restrict);
+            }
         }
 
         public static IEnumerable<IMutableEntityType> EntityTypes(this ModelBuilder builder)
