7e3e0b35dd21f755fdb1c00088e5696d0643a3af
Thomas Galliker <thomas@bluewin.ch>
2019-04-10 15:29:12 +0200

    Add ModelBuilderExtensions

--- EFCore.Toolkit/Auditing/Extensions/ModelBuilderExtensions.cs
+++ EFCore.Toolkit/Auditing/Extensions/ModelBuilderExtensions.cs
@@ -1,14 +1,47 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace EFCore.Toolkit.Auditing.Extensions
 {
     public static class ModelBuilderExtensions
     {
-        public static void AddConfiguration<TEntity>(
-            this ModelBuilder modelBuilder,
-            EntityTypeConfiguration<TEntity> entityConfiguration) where TEntity : class
+        public static void AddConfiguration<TEntity>(this ModelBuilder modelBuilder, EntityTypeConfiguration<TEntity> entityConfiguration) where TEntity : class
         {
             modelBuilder.Entity<TEntity>(entityConfiguration.Configure);
+        }
+
+        public static IEnumerable<IMutableEntityType> EntityTypes(this ModelBuilder builder)
+        {
+            return builder.Model.GetEntityTypes();
+        }
+
+        public static IEnumerable<IMutableProperty> Properties(this ModelBuilder builder)
+        {
+            return builder.EntityTypes().SelectMany(entityType => entityType.GetProperties());
+        }
+
+        public static IEnumerable<IMutableProperty> Properties<T>(this ModelBuilder builder)
+        {
+            return builder.EntityTypes().SelectMany(entityType => entityType.GetProperties().Where(x => x.ClrType == typeof(T)));
+        }
+
+        public static void Configure(this IEnumerable<IMutableEntityType> entityTypes, Action<IMutableEntityType> convention)
+        {
+            foreach (var entityType in entityTypes)
+            {
+                convention(entityType);
+            }
+        }
+
+        public static void Configure(this IEnumerable<IMutableProperty> propertyTypes, Action<IMutableProperty> convention)
+        {
+            foreach (var propertyType in propertyTypes)
+            {
+                convention(propertyType);
+            }
         }
     }
 }
