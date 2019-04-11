693bdfc5e1f1d9412e07aeadc5097219afacf825
Thomas Galliker <thomas@bluewin.ch>
2019-04-11 08:08:18 +0200

    Add PropertyBuilderExtensions.IsOptional

---
+++ EFCore.Toolkit/Extensions/PropertyBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCore.Toolkit.Abstractions.Extensions
+{
+    public static class PropertyBuilderExtensions
+    {
+        public static PropertyBuilder<TProperty> IsOptional<TProperty>(this PropertyBuilder<TProperty> propertyBuilder)
+        {
+            return propertyBuilder.IsRequired(required: false);
+        }
+    }
+}
