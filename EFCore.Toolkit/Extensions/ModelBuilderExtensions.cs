b9df96b03302257e4eafd15525883c72b2bf2ea7
Thomas Galliker <thomas@bluewin.ch>
2019-04-27 12:31:08 +0200

    Add SetDefaultDecimalPrecision

--- EFCore.Toolkit/Extensions/ModelBuilderExtensions.cs
+++ EFCore.Toolkit/Extensions/ModelBuilderExtensions.cs
@@ -77,22 +77,55 @@ namespace EFCore.Toolkit.Extensions
             }
         }
 
-        public static void SetDefaultStringMaxLength(this IEnumerable<IMutableProperty> propertyTypes, int maxLength = 4000, Func<IMutableEntityType, bool> includeEntityTypeFilter = null)
+#if !NETSTANDARD1_3
+        /// <summary>
+        ///  Configures all decimals and nullable decimals with a default <paramref name="precision"/> (maximum allowed digits in a number) and <paramref name="scale"/> (maximum allowed digits after the decimal point).
+        /// </summary>
+        public static void SetDefaultDecimalPrecision(this ModelBuilder modelBuilder, int precision, int scale)
         {
-            foreach (var p in propertyTypes)
+            foreach (var p in modelBuilder.Properties<decimal>().Concat(modelBuilder.Properties<decimal?>()))
             {
                 if (p.GetMaxLength() == null &&
-                    (includeEntityTypeFilter == null || includeEntityTypeFilter(p.DeclaringEntityType)) &&
                     p.IsConcurrencyToken == false &&
-                    string.Equals(p.Relational().ColumnType, "nvarchar(MAX)", StringComparison.OrdinalIgnoreCase) == false)
+                    (p.Relational().ColumnType == null || p.Relational().ColumnType.StartsWith("decimal(", StringComparison.InvariantCultureIgnoreCase) == false))
                 {
-                    Console.WriteLine($"SetMaxLength({maxLength}): {p.DeclaringEntityType.Name}.{p.Name}");
-                    p.SetMaxLength(maxLength);
+                    var columnType = $"decimal({precision},{scale})";
+                    Console.WriteLine($"SetColumnType({columnType}): {p.DeclaringEntityType.Name}.{p.Name}");
 
-                    p.Relational().ColumnType = "nvarchar";
+                    p.Relational().ColumnType = $"decimal({precision},{scale})";
                 }
 
             }
         }
+
+        /// <summary>
+        /// Configures all strings with a default <paramref name="maxLength"/>.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <param name="maxLength"></param>
+        /// <param name="includeEntityTypeFilter"></param>
+        public static void SetDefaultStringMaxLength(this ModelBuilder modelBuilder, int? maxLength = 4000, Func<IMutableEntityType, bool> includeEntityTypeFilter = null)
+        {
+            foreach (var p in modelBuilder.Properties<string>())
+            {
+                if (p.GetMaxLength() == null &&
+                    (includeEntityTypeFilter == null || includeEntityTypeFilter(p.DeclaringEntityType)) &&
+                    p.IsConcurrencyToken == false)
+                {
+                    var columnTypeInternal = "nvarchar";
+                    var maxLengthInternal = maxLength;
+                    if (string.Equals(p.Relational().ColumnType, "nvarchar(MAX)", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        columnTypeInternal = "nvarchar(MAX)";
+                        maxLengthInternal = null;
+                    }
+
+                    Console.WriteLine($"SetMaxLength({(maxLengthInternal == null ? "null" : $"{maxLengthInternal}")}): {p.DeclaringEntityType.Name}.{p.Name}");
+                    p.SetMaxLength(maxLengthInternal);
+                    p.Relational().ColumnType = columnTypeInternal;
+                }
+            }
+        }
+#endif
     }
 }
