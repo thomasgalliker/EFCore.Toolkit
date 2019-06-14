c73a719a02628955b5990ebc3874193fbd376e5b
Thomas Galliker <thomas@bluewin.ch>
2019-06-14 11:39:04 +0200

    Add ICreatedBy interface to handle data protection (wip)

---
+++ EFCore.Toolkit/Extensions/CreatedByExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using EFCore.Toolkit.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Toolkit.Extensions
+{
+    public static class CreatedByExtensions
+    {
+        public static void SetCreatedBy<TKey>(this DbContext context, TKey userId)
+        {
+            foreach (var entityEntry in context.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added))
+            {
+                if (entityEntry.Entity is ICreatedBy<TKey> entityToMark)
+                {
+                    entityToMark.CreatedBy = userId;
+                }
+            }
+        }
+    }
+
+}
