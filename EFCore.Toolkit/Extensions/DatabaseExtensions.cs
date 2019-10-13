4cd83653df69ff4adad8c4de117c7bab848028f6
Thomas Galliker <thomas@bluewin.ch>
2019-10-13 14:16:31 +0200

    try-catch around KillConnectionsToTheDatabase

--- EFCore.Toolkit/Extensions/DatabaseExtensions.cs
+++ EFCore.Toolkit/Extensions/DatabaseExtensions.cs
@@ -7,28 +7,27 @@ namespace EFCore.Toolkit.Extensions
 {
     public static class DatabaseExtensions
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2100:Review SQL queries for security vulnerabilities")]
         public static void KillConnectionsToTheDatabase(this DatabaseFacade database)
         {
-            var dbConnection = database.GetDbConnection();
-            var databaseName = dbConnection.Database;
-            const string sqlFormat = @"
-             USE master; 
-
-             DECLARE @databaseName VARCHAR(50);
-             SET @databaseName = '{0}';
-
-             declare @kill varchar(8000) = '';
-             select @kill=@kill+'kill '+convert(varchar(5),spid)+';'
-             from master..sysprocesses 
-             where dbid=db_id(@databaseName);
-
-             exec (@kill);";
-
-            var sql = string.Format(sqlFormat, databaseName);
-
             try
             {
+                var dbConnection = database.GetDbConnection();
+                var databaseName = dbConnection.Database;
+                const string sqlFormat = @"
+                     USE master; 
+
+                     DECLARE @databaseName VARCHAR(50);
+                     SET @databaseName = '{0}';
+
+                     declare @kill varchar(8000) = '';
+                     select @kill=@kill+'kill '+convert(varchar(5),spid)+';'
+                     from master..sysprocesses 
+                     where dbid=db_id(@databaseName);
+
+                     exec (@kill);";
+
+                var sql = string.Format(sqlFormat, databaseName);
+
                 using (var command = dbConnection.CreateCommand())
                 {
                     command.CommandText = sql;
