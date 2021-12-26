78be412e73d5ab634a4970f980805be662f2f474
Thomas Galliker <thomas@bluewin.ch>
2021-12-26 11:48:38 +0100

    Add extension method HasRowVersion

---
+++ EFCore.Toolkit/Extensions/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCore.Toolkit.Extensions
+{
+    public static class EntityTypeBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a column "RowVersion" of type byte[] used for optimistic concurrency detection.
+        /// </summary>
+        public static PropertyBuilder<byte[]> HasRowVersion<TProperty>(this EntityTypeBuilder entityTypeBuilder)
+        {
+            return entityTypeBuilder.Property<byte[]>("RowVersion")
+                .IsRowVersion()
+                .HasColumnName("RowVersion");
+        }
+    }
+}
