f03651a2c260f5fc20634e9f00e3898b50a0cafd
thomasgalliker <thomas@bluewin.ch>
2025-12-10 09:02:07 +0100

    Add new extension methods

--- EFCore.Toolkit.Abstractions/Extensions/GenericRepositoryExtensions.cs
+++ EFCore.Toolkit.Abstractions/Extensions/GenericRepositoryExtensions.cs
@@ -3,17 +3,17 @@
     public static class GenericRepositoryExtensions
     {
         /// <summary>
-        ///     Marks the the entity with the given primary key as Deleted such that it will be deleted from the database when
-        ///     SaveChanges is called. Note that the entity must exist in the context in some other state before this method
-        ///     is called.
+        /// Marks the the entity with the given primary key as Deleted such that it will be deleted from the database when
+        /// SaveChanges is called. Note that the entity must exist in the context in some other state before this method
+        /// is called.
         /// </summary>
         /// <returns> The entity that has been removed.</returns>
         /// <remarks>
-        ///     Note that if the entity exists in the context in the Added state, then this method
-        ///     will cause it to be detached from the context.  This is because an Added entity is assumed not to
-        ///     exist in the database such that trying to delete it does not make sense.
+        /// Note that if the entity exists in the context in the Added state, then this method
+        /// will cause it to be detached from the context.  This is because an Added entity is assumed not to
+        /// exist in the database such that trying to delete it does not make sense.
         /// </remarks>
-        public static T RemoveById<T>(this IGenericRepository<T> repository, params object[] ids)
+        public static TEntity RemoveById<TEntity>(this IGenericRepository<TEntity> repository, params object[] ids)
         {
             var entity = repository.FindById(ids);
             if (entity == null)
@@ -24,15 +24,20 @@
             return repository.Remove(entity);
         }
 
+        public static IEnumerable<TEntity> RemoveByExternalId<TEntity>(this IGenericRepository<TEntity> queryable, Guid externalId) where TEntity : IExternalIdentifiable
+        {
+            return queryable.RemoveAll(i => i.ExternalId == externalId);
+        }
+
         /// <summary>Removes all entities that match the conditions defined by the given predicate.</summary>
         /// <returns>The removed entities.</returns>
         /// <param name="predicate">The expression that defines the conditions of the elements to remove.</param>
         /// <exception cref="T:System.ArgumentNullException">
-        ///     <paramref name="predicate" /> is null.
+        /// <paramref name="predicate" /> is null.
         /// </exception>
-        public static IEnumerable<T> RemoveAll<T>(this IGenericRepository<T> repository, Func<T, bool>? predicate = null)
+        public static IEnumerable<TEntity> RemoveAll<TEntity>(this IGenericRepository<TEntity> repository, Func<TEntity, bool>? predicate = null)
         {
-            IEnumerable<T> query = repository.Get();
+            IEnumerable<TEntity> query = repository.Get();
             if (predicate != null)
             {
                 query = query.Where(predicate);
---
+++ EFCore.Toolkit.Abstractions/Extensions/QueryableExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace EFCore.Toolkit.Abstractions.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static int? FindIdByExternalId<TEntity>(this IQueryable<TEntity> queryable, Guid externalId) where TEntity : IExternalIdentifiable, IIdentifiable
+        {
+            var entity = queryable
+                .Select(x => new { x.Id, x.ExternalId })
+                .SingleOrDefault(i => i.ExternalId == externalId);
+
+            if (entity == null)
+            {
+                return null;
+            }
+
+            return entity.Id;
+        }
+
+        public static TEntity FindByExternalId<TEntity>(this IQueryable<TEntity> queryable, Guid externalId) where TEntity : IExternalIdentifiable
+        {
+            return queryable.SingleOrDefault(i => i.ExternalId == externalId);
+        }
+
+        public static int GetNextId<TEntity>(this ICollection<TEntity> items) where TEntity : IIdentifiable
+        {
+            if (items.Any())
+            {
+                var lastId = items.Max(t => t.Id);
+                var nextId = lastId + 1;
+                return nextId;
+            }
+
+            return 1;
+        }
+    }
+}
