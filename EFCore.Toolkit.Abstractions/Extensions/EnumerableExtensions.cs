4d6bf0179d41dec6ff58b394eb9c1bd603090f41
thomasgalliker <thomas@bluewin.ch>
2025-12-18 22:35:57 +0100

    Add reset identifiers methods

--- EFCore.Toolkit.Abstractions/Extensions/EnumerableExtensions.cs
+++ EFCore.Toolkit.Abstractions/Extensions/EnumerableExtensions.cs
@@ -2,7 +2,6 @@
 {
     public static class EnumerableExtensions
     {
-
         public static int GetNextId<T>(this IEnumerable<T> items) where T : IIdentifiable
         {
             if (items.Any())
---
+++ EFCore.Toolkit.Abstractions/Extensions/IdentifiableExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace EFCore.Toolkit.Abstractions.Extensions
+{
+    public static class IdentifiableExtensions
+    {
+        public static IEnumerable<T> ResetIds<T>(this IEnumerable<T> source) where T : IIdentifiable
+        {
+            foreach (var item in source)
+            {
+                item.Id = 0;
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<T> ResetExternalIds<T>(this IEnumerable<T> source) where T : IExternalIdentifiable
+        {
+            foreach (var item in source)
+            {
+                item.ExternalId = Guid.Empty;
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<T> ResetAllIds<T>(this IEnumerable<T> source) where T : IIdentifiable, IExternalIdentifiable
+        {
+            foreach (var item in source)
+            {
+                item.Id = 0;
+                item.ExternalId = Guid.Empty;
+                yield return item;
+            }
+        }
+    }
+}
