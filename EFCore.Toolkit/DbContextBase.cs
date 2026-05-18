9600c62c8cc7ef57d12e91a75be146cd9965bada
thomasgalliker <thomas@bluewin.ch>
2026-05-18 21:43:12 +0200

    Cleanup

--- EFCore.Toolkit/DbContextBase.cs
+++ EFCore.Toolkit/DbContextBase.cs
@@ -6,7 +6,6 @@ using EFCore.Toolkit.Exceptions;
 using EFCore.Toolkit.Extensions;
 using EFCore.Toolkit.Interceptors;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EFCore.Toolkit
